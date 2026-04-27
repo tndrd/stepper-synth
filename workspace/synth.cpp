3dba5a3976e16c31bcdcef8a9c645412c63a60aa
Vladimir Lekhterev <vovalekhterev@gmail.com>
2026-04-27 10:46:39 +0300

    Implement vibrato & stutter

--- workspace/synth.cpp
+++ workspace/synth.cpp
@@ -1,5 +1,6 @@
 #include "synth.hpp"
 
+#define _USE_MATH_DEFINES
 #include <cmath>
 #include <vector>
 
@@ -7,17 +8,16 @@
 #include "stm32f407xx.h"
 
 StepperSynth::Drive::Drive(const Pins& pins)
-    : m_stp{pins.stp}, m_dir{pins.dir}, m_period_us{0}, m_counter{0} {}
+    : m_stp{pins.stp}, m_dir{pins.dir}, m_note{0}, m_counter{0} {}
 
 StepperSynth::StepperSynth(const std::vector<Pins>& pin_array)
     : m_base_note{MIDI::Notes::A4},
-      m_base_freq{440.0l},
+      m_base_freq{440.0},
       m_fine_tune{0},
       m_transpose{0},
-      m_pitch_wheel{0},
-      m_velo_threshold{100},
       m_enable{true} {
   for (const auto& pins : pin_array) m_drives.emplace_back(pins);
+  for (int i = 0; i < 12; ++i) m_note_coeffs[i] = pow(2, float(i) / 12);
 }
 
 void StepperSynth::enable(bool value) { m_enable = value; }
@@ -26,36 +26,59 @@ void StepperSynth::update(uint16_t dt_us) {
   assert(dt_us);
 
   for (auto& drive : m_drives) {
-    bool value = false;
-    uint16_t period = drive.m_period_us / dt_us;
+    if (!m_enable || drive.m_note == 0) {
+      drive.m_stp.write(false);
+      drive.m_counter++;
+      continue;
+    }
+    
+    if (drive.m_counter == ~uint32_t(0))
+      drive.m_counter = 0;
 
-    if (m_enable && period != 0) {
-      drive.m_counter += 1;
-      drive.m_counter %= period;
-
-      // 50% duty cycle
-      value = drive.m_counter < (period / 2);
+    // Vibrato modulation
+    float mod = drive.m_fx.vibrato_depth;
+    if (drive.m_fx.vibrato_period_ms) {
+      uint32_t period = (drive.m_fx.vibrato_period_ms * 1000) / dt_us;
+      float rad = float(drive.m_counter % period) / float(period);
+      mod *= sinf(2.f * M_PI * rad);
     }
 
+    bool value;
+    uint16_t period;
+    
+    if (drive.m_counter * dt_us < drive.m_fx.stutter_duration_ms * 1000)
+    {
+      period = drive.m_fx.stutter_period_us;
+      value = ((drive.m_counter * dt_us) % period) < (period / 2);
+      drive.m_dir.write(value);
+    }
+    
+    period = getNotePeriodUs(drive.m_note, mod);
+    value = ((drive.m_counter * dt_us) % period) < (period / 2);
+
     drive.m_stp.write(value);
+    drive.m_counter += 1;
   }
 }
 
-void StepperSynth::noteOn(uint8_t id, uint8_t note, uint8_t velocity) {
+void StepperSynth::playNote(uint8_t id, uint8_t note) {
   assert(id < m_drives.size());
-  auto& drive = m_drives[id];
-
-  if (velocity == 0)
-    drive.m_period_us = 0;
-  else {
-    if (velocity >= m_velo_threshold) drive.m_dir.toggle();
-    drive.m_period_us = getNotePeriodUs(note);
-  }
+  m_drives[id].m_note = note;
+  m_drives[id].m_counter = ~uint32_t(0);
 }
 
-uint16_t StepperSynth::getNotePeriodUs(uint8_t note) {
-  double power = double(m_base_note - note - m_transpose) + m_pitch_wheel;
-  double dt = 1e6l / (m_base_freq + m_fine_tune);
+auto StepperSynth::fx(uint8_t id) -> FX& {
+  assert(id < m_drives.size());
+  return m_drives[id].m_fx;
+}
 
-  return dt * pow(2, power / 12);
+uint16_t StepperSynth::getNotePeriodUs(uint8_t note, float mod) {
+  float dt = 1e6l / (m_base_freq + m_fine_tune);
+
+  uint8_t dist = m_base_note - note - m_transpose;
+  assert(dist >= 0);
+  float t_us = dt * (1 << dist / 12) * m_note_coeffs[dist % 12] * powf(2, mod / 12);;
+  float freq = 1e6f / t_us;
+
+  return t_us;
 }
--- workspace/synth.hpp
+++ workspace/synth.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+
 #include "common.hpp"
 #include "midi.hpp"
 
@@ -11,26 +12,37 @@ struct StepperSynth final {
     PinBase dir;
   };
 
+  struct FX {
+    float vibrato_depth = 0;        // In semitones
+    uint16_t vibrato_period_ms = 0;  // Period
+
+    uint16_t stutter_period_us = 0;
+    uint16_t stutter_duration_ms = 0;
+  };
+
  private:
   struct Drive {
     GPIOPinOut m_stp;
     GPIOPinOut m_dir;
 
-    uint16_t m_period_us;
-    uint16_t m_counter;
+    volatile uint8_t m_note;
+    volatile uint32_t m_counter;
+
+    FX m_fx;
 
     Drive(const Pins& pins);
   };
 
  private:
   uint8_t m_base_note;
-  double m_base_freq;
-  double m_fine_tune;
+  float m_base_freq;
+  float m_fine_tune;
 
  private:
   int8_t m_transpose;
-  double m_pitch_wheel;
-  uint8_t m_velo_threshold;
+
+  // 2^{0/12}, 2^{1/12}, ..., 2^{11/12}
+  std::array<float, 12> m_note_coeffs;
 
  private:
   bool m_enable;
@@ -41,9 +53,12 @@ struct StepperSynth final {
 
   void enable(bool value);
   void update(uint16_t dt_us);
-  void noteOn(uint8_t id, uint8_t note, uint8_t velocity);
+
+  void playNote(uint8_t id, uint8_t note);
+
+  FX& fx(uint8_t id);
 
  private:
   // 12TET conversion
-  uint16_t getNotePeriodUs(uint8_t note);
+  uint16_t getNotePeriodUs(uint8_t note, float mod);
 };
